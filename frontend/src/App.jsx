faae24c444f424e681536dd7f3abe39f60666fb1
ReeVNaR <ranveerghorpade16@gmail.com>
2025-03-17 02:23:53 +0530

    Ranveer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
-import AdminPage from './pages/AdminPage';
-import ChatPage from './pages/ChatPage';
 import { ThemeProvider } from './context/ThemeContext';
 
 const PrivateRoute = ({ children }) => {
@@ -10,32 +8,12 @@ const PrivateRoute = ({ children }) => {
   return token ? children : <Navigate to="/login" />;
 };
 
-const AdminRoute = ({ children }) => {
-  const isAdmin = localStorage.getItem('isAdmin') === 'true';
-  return isAdmin ? children : <Navigate to="/" />;
-};
-
 function App() {
   return (
     <ThemeProvider>
       <Router>
         <Routes>
-          <Route path="/" element={
-            <Navigate to={localStorage.getItem('token') ? '/home' : '/login'} replace />
-          } />
-          <Route path="/login" element={
-            localStorage.getItem('token') ? <Navigate to="/home" replace /> : <LoginPage />
-          } />
-          <Route path="/admin" element={
-            <AdminRoute>
-              <AdminPage />
-            </AdminRoute>
-          } />
-          <Route path="/chat/:friendId" element={
-            <PrivateRoute>
-              <ChatPage />
-            </PrivateRoute>
-          } />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/*" element={
             <PrivateRoute>
               <HomePage />
